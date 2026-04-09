bf8687fe92e8eb097b2a73d43c1012886b59119e
FirasKahlaoui <kahlaouifiras2017@gmail.com>
2026-04-09 13:40:31 +0100

    Update application configuration and .gitignore files
    - Exclude specific security auto-configuration classes in TrainingManagementApplication
    - Add database connection properties to application.properties
    - Update .gitignore to include cp.txt in backend

--- backend/src/main/java/com/formationmanagement/training_management/TrainingManagementApplication.java
+++ backend/src/main/java/com/formationmanagement/training_management/TrainingManagementApplication.java
@@ -2,8 +2,17 @@ package com.formationmanagement.training_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { 
+    SecurityAutoConfiguration.class, 
+    SecurityFilterAutoConfiguration.class,
+    ServletWebSecurityAutoConfiguration.class,
+    UserDetailsServiceAutoConfiguration.class
+})
 public class TrainingManagementApplication {
 
 	public static void main(String[] args) {
